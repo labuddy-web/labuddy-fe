877030621e6844683966b57376c84e60a59e6f97
limgahyun <limgh2002@gmail.com>
2025-04-01 22:34:30 +0900

    [feat] login ui

---
+++ app/login/page.tsx
@@ -0,0 +1,11 @@
+import Login from "./Login";
+
+const loginPage = () => {
+  return (
+    <div className="flex flex-col w-full h-[calc(100vh-84px)] justify-center items-center">
+      <Login />
+    </div>
+  );
+};
+
+export default loginPage;
