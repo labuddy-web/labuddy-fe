8efbfaf0d00a9f55cc40a0ba5f567d872d63dcc1
limgahyun <limgh2002@gmail.com>
2025-04-01 17:54:37 +0900

    [fix] tailwind color setting 수정

---
+++ app/_main/MainSection.tsx
@@ -0,0 +1,10 @@
+const MainSection = () => {
+  return (
+    <div className="flex w-full h-[884px] bg-labuddy px-[320px]">
+      <p>LabBuddy</p>
+      <p>: Extract Reagent & Equipment Info from Research Papers</p>
+    </div>
+  );
+};
+
+export default MainSection;
