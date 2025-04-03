b9497d4bf6d7fe92cf97bc86078d6ef7ab33d29f
limgahyun <limgh2002@gmail.com>
2025-04-04 00:10:03 +0900

    [refactor] upload icon 컴포넌트화

--- app/_main/MainSection.tsx
+++ app/_main/MainSection.tsx
@@ -1,3 +1,4 @@
+import UploadIcon from "@/components/icons/UploadIcon";
 import { FileInput, Label } from "flowbite-react";
 
 const MainSection = () => {
@@ -25,21 +26,7 @@ const MainSection = () => {
 
       <Label className="flex flex-col items-center justify-center w-[760px] max-w-full h-[120px] border-2 border-white border-dashed rounded-lg cursor-pointer bg-transparent dark:bg-transparent dark:border-white">
         <div className="flex flex-col items-center justify-center">
-          <svg
-            className="w-8 h-8 mb-4 text-white dark:text-white"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
+          <UploadIcon />
           <p className="text-sm text-white dark:text-white">
             <span className="font-semibold">Click to upload</span> or drag and
             drop
