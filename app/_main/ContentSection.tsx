04c655366889601b947bd2e7640069004ebe6b70
limgahyun <limgh2002@gmail.com>
2025-04-08 02:18:36 +0900

    [style] roca font 추가

--- app/_main/ContentSection.tsx
+++ app/_main/ContentSection.tsx
@@ -3,7 +3,7 @@ import { contentList, ourMission, footer } from "./contentData";
 
 const ContentSection = () => {
   return (
-    <section className="flex flex-col w-full h-auto justify-center items-center px-[32px] lg:px-[120px] xl:px-[320px] py-[180px] gap-[120px]">
+    <section className="font-roca flex flex-col w-full h-auto justify-center items-center px-[32px] lg:px-[120px] xl:px-[320px] py-[180px] gap-[120px]">
       {contentList.map((item, index) => (
         <MainContentBox key={index} title={item.title} content={item.content} />
       ))}
